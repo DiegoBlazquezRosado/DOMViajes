c542a4a03a6544aa733c7d3e8e3f925d9096ca7e
DiegoBlazquezRosado <diegoblazquezr@gmail.com>
2024-05-18 20:46:10 +0200

    destinos finished

--- scripts.js
+++ scripts.js
@@ -1,24 +1,55 @@
 //DOM Viajes
 
-const divBienvenidos = document.getElementById('bienvenidos');
+//let fragment = document.createDocumentFragment();
 
-const insertBanner = () => {
+//Bienvenidos
+const insertBienvenidos = () => {
 
-    const arrBanner = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg'];
-    let randomBanner = Math.floor(Math.random()*arrBanner.length);
+    const arrBienvenidos = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg'];
+    let randomBienvenidos = Math.floor(Math.random()*arrBienvenidos.length);
 
-    const arrTextBanner = [];
-    for (let i=0; i<arrBanner.length; i++) {
-        arrTextBanner.push(`Image ${i+1}`);
+    const arrTextBienvenidos = [];
+    for (let i=0; i<arrBienvenidos.length; i++) {
+        arrTextBienvenidos.push(`Image ${i+1}`);
     }
 
-    const imgBanner = document.createElement('img');
-    divBienvenidos.appendChild(imgBanner);
+    const divBienvenidos = document.getElementById('bienvenidos');
+    const imgBienvenidos = document.createElement('img');
+    divBienvenidos.appendChild(imgBienvenidos);
 
-    imgBanner.src = `./banner/${arrBanner[randomBanner]}`;
-    imgBanner.title = arrTextBanner[randomBanner];
-    imgBanner.alt = arrTextBanner[randomBanner];
+    imgBienvenidos.src = `./banner/${arrBienvenidos[randomBienvenidos]}`;
+    imgBienvenidos.title = arrTextBienvenidos[randomBienvenidos];
+    imgBienvenidos.alt = arrTextBienvenidos[randomBienvenidos];
 
 }
+insertBienvenidos();
 
-insertBanner();
+
+//Recomendados
+const insertRecomendados = () => {
+
+
+
+}
+insertRecomendados();
+
+
+//Destinos
+const insertDestinos = () => {
+
+    const arrDestinos = ['Australia', 'Suiza', 'País Vasco', 'Paises Bajos', 'Dinamarca'];
+
+    const divDestinos = document.getElementById('destinos');
+    const selectDestinos = document.createElement('select');
+    divDestinos.appendChild(selectDestinos);
+
+    arrDestinos.forEach((destino) => {
+        const optionDestinos = document.createElement('option');
+        //fragment.appendChild(optionDestinos);
+        selectDestinos.appendChild(optionDestinos);
+        optionDestinos.value = destino;
+        optionDestinos.text = destino;
+    });
+
+}
+insertDestinos();
